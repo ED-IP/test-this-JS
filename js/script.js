68e605c8da4b76c71744a0f4d4eebe16bf7506f0
ED-IP <edip_courses@outlook.es>
2020-07-03 08:47:53 +0000

    fixed several typos

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$(documnet).ready(function(){
+$(document).ready(function(){
     $(".box").on("click", function(){
         var classNames = $(this).attr("class").split(" ");
         $("." + classNames[1]).css("background-color", "red");
